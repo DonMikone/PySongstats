da4bda78d8a8f87b49ae32633c13c97164b4b07a
Mike Kremer <mikelsoft@gmail.com>
2025-06-26 16:48:19 +0200

    Fixed testing URL.

--- songstats/constants.py
+++ songstats/constants.py
@@ -1,2 +1,2 @@
 BASE_URL_PROD = "https://api.songstats.com/enterprise/v1/"
-BASE_URL_TEST = "https://stoplight.io/mocks/songstats/api/12173793/v1/"
+BASE_URL_TEST = "https://stoplight.io/mocks/songstats/api/12173793/"
